556052ff0990e151868a66c3ab0a2c5fc89d40e3
afry72 <ajfrye72@gmail.com>
2024-03-14 22:51:50 -0400

    completed functionality of the website

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 //var for dom elements
+//vars for main part of quiz
 var startButton = document.getElementById("start");
 var timer = document.getElementById("timer");
 var quiz = document.getElementById("questions");
@@ -11,20 +12,28 @@ var startParagraph = document.getElementById("startParagraph");
 var currentIndex = 0;
 var recentScore = 0;
 
+//vars for ending the game
 var gameEndScreen = document.getElementById("endScreen");
 var finalScore = document.getElementById("finalScore");
 var submit = document.getElementById("submitButton");
 
+//vars for high score screen
 var highScoreScreen = document.getElementById("highScoreScreen");
 var scoreList = document.getElementById("scoreList");
 var clearScore = document.getElementById("clearScore");
-var resetQuiz = document.getElementById("reset");
+var resetQuiz = document.getElementById("resetQuiz");
 
 // timer var
 // score variable
 var score = 0;
 var time = 75;
 
+//storing scores inside local storage
+var storageScore = localStorage.getItem("storageScore");
+var storageInitials = localStorage.getItem("storageInitials");
+var scoreList = document.getElementById("scoreList");
+
+//Questions array
 var questions = [
     {
         header: "Commonly used date types DO NOT include:",
@@ -55,6 +64,7 @@ var questions = [
 
 ];
 
+//function to create a timer when start button is hit
 function gameTime () {
     var refreshTimer = setInterval(timeIndex, 1000);
 
@@ -71,15 +81,11 @@ function gameTime () {
 
 }
 
-//timer score event listener high score
-//event listener
+//function that sets entire website into motion
 function startGame () {
-    //hide start button
     generateQuestions ();
-    //start timer
     timer.textContent = time;
     currentScore.textContent = score;
-    //hideButton ();
     startButton.setAttribute("class", "hide");
     startParagraph.setAttribute("class", "hide");
     timeHeader.removeAttribute("class", "none");
@@ -88,6 +94,7 @@ function startGame () {
 
 }
 
+//function to check if the chosen answer is correct and then effects time and score accordingly
 function nextQuestion (event) {
     var clickedAnswer = event.target.textContent;
     console.log("click", clickedAnswer);
@@ -100,7 +107,7 @@ function nextQuestion (event) {
     }
 
     if (clickedAnswer == rightAnswer) {
-       score = score+11;
+       score = score+20;
        currentScore.textContent = score;
     } else {
         time = time-10;
@@ -123,6 +130,7 @@ function nextQuestion (event) {
 
 }
 
+//function that ends the game and brings you into the end screen
 function endGame () {
     time = 0;
     console.log("entered endgame")
@@ -135,6 +143,7 @@ function endGame () {
     
 }
 
+//function that provides questions from the questions array for the quiz to use
 function generateQuestions () {
     var displayedQuestion = questions[currentIndex];
 
@@ -155,21 +164,44 @@ function generateQuestions () {
 
 }
 
-
+//function that takes the provided initials and score and embeds them in local storage
 function sendToStorage () {
     console.log("submitted");
-    gameEndScreen.setAttribute("class", "hide")
-    highScoreScreen.removeAttribute("class", "none")
+    gameEndScreen.setAttribute("class", "hide");
+    highScoreScreen.removeAttribute("class", "none");
+    var initialsInput = document.getElementById("initialsText").value;
+    console.log("info logged", initialsInput);
+    //if score is greater than stored score then replace stored score
+    var testScoreData = JSON.parse(localStorage.getItem('highScoreStorage'));
+    if (testScoreData == null || score > testScoreData.score) {
+        var highScoreStorage = {score, initialsInput};
+        localStorage.setItem('highScoreStorage', JSON.stringify(highScoreStorage));
+    }
+   // localStorage.setItem(score, storageScore);
+    //localStorage.setItem(initialsInput, storageInitials);
+    renderStorage();
+
 }
 
+function renderStorage() {
+    var highScoreData = JSON.parse(localStorage.getItem('highScoreStorage'));
+    console.log(highScoreData);
+    //highScoreData.score
+    scoreList.textContent = highScoreData.score+highScoreData.initialsInput;
+} 
+
+//function to start quiz over
 function refreshQuiz () {
-    console.log("refresh quiz")
+    console.log("refresh quiz");
 }
 
+//function that resets score board
 function refreshScore () {
-    console.log("refreshScore")
+    console.log("refreshScore");
+    localStorage.removeItem('highScoreStorage');
 }
 
+// event listeners that execute the various function
 clearScore.onclick = refreshScore;
 resetQuiz.onclick = refreshQuiz;
 submit.onclick = sendToStorage;
